4eaf12f0785ff462db6ccb1f230a7ebf23922033
yangjingcai <yjc454450448@sina.com>
2018-08-12 08:20:06 +0800

    First Commit

---
+++ LearnGitDemo/LearnGitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearnGItDemo
+//
+//  Created by yangjingcai on 2018/8/12.
+//  Copyright © 2018年 yangjingcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LearnGitDemo/LearnGitDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LearnGItDemo
+//
+//  Created by yangjingcai on 2018/8/12.
+//  Copyright © 2018年 yangjingcai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
